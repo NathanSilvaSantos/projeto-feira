0a687ddb426e7ee0d9e6e523afe7e1d30030ebe0
Nathan <nathan013silva@gmail.com>
2019-07-14 22:48:15 -0300

    inserindo o bot

--- pagina.py
+++ pagina.py
@@ -1,14 +1,38 @@
 from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
 
+alan = ChatBot('Alan')
+
+treinador = ListTrainer(alan)
 app = Flask(__name__)
 
 value = "Olá"
 
 @app.route('/pagina.py',)
 @app.route('/',)
+def treinar():
+    for arquivo in os.listdir(r'chats'): # percorrer todos os arquivos
+	    linhas = open(r'chats/' + arquivo, 'r').readlines() # ler todas as linhas
+	    treinador.train(linhas)
+
+
+@app.route('/pagina.py',)
+@app.route('/',)
 def hello():
-    msg = request.args.get('mensagem', value)
-    return render_template('index.html', **{'msg': msg})
+    treinar()
+    try:
+        msg = request.args.get('mensagem', value)
+
+        resposta = alan.get_response(msg)
+        if float(resposta.confidence) >= 0.3:
+            return render_template('index.html', **{'alan': resposta})
+        else:
+            return render_template('index.html', **{'msg': msg})
+    except Exception as e:
+        return render_template('index.html', **{'msg': e})
+
 
 if __name__ =='__main__':
     app.run()
