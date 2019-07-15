6cec7d2d71cf369b50ac42fcb1df4d19e603616f
Nathan <nathan013silva@gmail.com>
2019-07-15 18:38:22 -0300

    comentando o coódigo

--- pagina.py
+++ pagina.py
@@ -1,36 +1,34 @@
-from flask import Flask, render_template, request
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from flask import Flask, render_template, request #importa os módulos do flask
+from chatterbot import ChatBot # importa o chatbot
+from chatterbot.trainers import ListTrainer # importa o método de treino
 import os
 
-alan = ChatBot('Alan')
+alan = ChatBot('Alan') # inicia o bot
 
-treinador = ListTrainer(alan)
-app = Flask(__name__)
+treinador = ListTrainer(alan) # define o método de treino
+app = Flask(__name__) # inicia a aplicação flask
 
-value = "Olá"
+value = "Olá" 
 
-def treinar():
-    for arquivo in os.listdir(r'chats'): # percorrer todos os arquivos
-	    linhas = open(r'chats/' + arquivo, 'r').readlines() # ler todas as linhas
-	    treinador.train(linhas)
+for arquivo in os.listdir(r'chats'): # percorrer todos os arquivos
+	linhas = open(r'chats/' + arquivo, 'r').readlines() # ler todas as linhas
+	treinador.train(linhas) # treina o conteúdo das linhas
 
 
 @app.route('/pagina.py',)
 @app.route('/',)
 def hello():
-    treinar()
     try:
-        msg = request.args.get('mensagem', value)
+        msg = request.args.get('mensagem', value) # recebe a mensagem do usuário
 
-        resposta = alan.get_response(msg)
-        if float(resposta.confidence) >= 0.3:
-            return render_template('index.html', **{'alan': resposta})
+        resposta = alan.get_response(msg) # gera uma resposta
+        if float(resposta.confidence) >= 0.3: # verifica se a resposta tem mais de 30% de confiança
+            return render_template('index.html', **{'alan': resposta}) # retorna a resposta
         else:
-            return render_template('index.html', **{'msg': msg})
+            return render_template('index.html', **{'alan': 'Desculpe, ainda não sei responder essa pergunta'}) # Resposta padrão 
     except Exception as e:
-        return render_template('index.html', **{'msg': e})
+        return render_template('index.html', **{'Alan': 'Ocorreu um erro durante a execução:'+e}) # Retorna mensagem de erro
 
 
 if __name__ =='__main__':
-    app.run()
+    app.run() # Inicia a aplicação
